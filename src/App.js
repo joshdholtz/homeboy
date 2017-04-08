a48b20d8e27e0fa9ddbe9fe8c587a656e8c8c41f
Josh Holtz <josh@rokkincat.com>
2017-04-08 15:29:34 -0500

    Changed words

--- src/App.js
+++ src/App.js
@@ -266,7 +266,7 @@ class App extends Component {
           }
         </ul>
         <div>
-          <Button bsStyle="danger" onClick={() => (this.handleDeleteConfig())}>Bye, Felicia (aka local storage config)</Button>
+          <Button bsStyle="danger" onClick={() => (this.handleDeleteConfig())}>Bye, Felicia (aka saved config)</Button>
         </div>
         <div className="image-info">
           Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
