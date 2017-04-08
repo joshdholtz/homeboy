be6fc17d09e18a4f5aa785f534ae28c1916eeea1
Josh Holtz <josh@rokkincat.com>
2017-04-08 15:26:25 -0500

    100% complete

--- src/App.js
+++ src/App.js
@@ -227,6 +227,13 @@ class App extends Component {
     }
   }
   
+  handleDeleteConfig() {
+    if (confirm("Do you want to bye felicia?")) {
+      localStorage.setObject('config', null)
+      window.location.reload()
+    }
+  }
+  
   renderConfig() {
     return (
       <Form onSubmit={(e) => {this.handleConfigSave(e)}}>
@@ -258,6 +265,9 @@ class App extends Component {
             })
           }
         </ul>
+        <div>
+          <Button bsStyle="danger" onClick={() => (this.handleDeleteConfig())}>Bye, Felicia (aka local storage config)</Button>
+        </div>
         <div className="image-info">
           Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
         </div>
